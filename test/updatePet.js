967d091b70ca53b5019744373030f87d7f29516d
sorbon.imomnazarov <sorbon.imomnazarov@accenture.com>
2022-12-27 22:01:15 +0100

    Update files

--- test/updatePet.js
+++ test/updatePet.js
@@ -2,28 +2,30 @@ import { expect } from "chai";
 import request from "../config/common";
 
 describe("Updating details of a given pet", () => {
-  let petId, petStatus;
+
+  const data = {
+    id: Math.floor(Math.random() * 999999999),
+    status: "available"
+  };
 
   // Creating a new pet
   it("POST /pet", async () => {
-    const data = {
-      id: Math.floor(Math.random() * 999999999),
-      status: "available",
-    };
+    data.id;
+    data.status;
 
     const res = await request
       .post("pet")
       .send(data)
       // Verifying that the status code of the response is 200
       .expect(200);
+
+      console.log(res.body);
   });
 
   // Getting the details of the newly created pet
   it("PUT /pet", async () => {
-    const data = {
-      id: Math.floor(Math.random() * 333333),
-      status: "sold",
-    };
+    data.id = Math.floor(Math.random() * 333333);
+    data.status = "sold";
 
     const res = await request
       .put("pet")
@@ -31,11 +33,10 @@ describe("Updating details of a given pet", () => {
       // Verifying that the status code of the response is 200
       .expect(200);
 
-    // assigning the data
-    petId = res.body.id;
-    petStatus = res.body.status;
+      console.log(res.body);
+
     // Verifying that the new pet has the details given previously (id and status)
-    expect(res.body.id).to.eq(petId);
-    expect(res.body.status).to.eq(petStatus);
+    expect(res.body.id).to.eq(data.id);
+    expect(res.body.status).to.eq(data.status);
   });
 });
